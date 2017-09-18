92eab6316a9fcd171cca2cf0cf9e5024f12e66d1
Xin Wang <wangxin94@163.com>
2017-09-17 22:57:48 -0400

    question 5

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -43,10 +43,34 @@ def q1_2():
     plt.plot(x_new,training_smooth, label='training error')
     plt.plot(x_new,test_smooth, label='test error')
     # show legend
+    plt.legend()
+    plt.show()
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def q5():
+    from scipy.special import expit
+    x_axis = np.array([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, \
+                        4, 5, 6, 7, 8, 9, 10])
+    tanh = np.tanh(x_axis)
+    sig = expit(x_axis)
+    x_new = np.linspace(x_axis.min(),x_axis.max(),300) #300 represents number of points to make between T.min and T.max
+    tanh_smooth = spline(x_axis,tanh,x_new)
+    sig_smooth = spline(x_axis,sig,x_new)
+    # remove the x, y label
+    # plt.xticks([])
+    # plt.yticks([])
+    # add x, y label to plot
+    plt.xlabel("Training Set Size")
+    plt.ylabel("Error Rate")
+    plt.plot(x_new,tanh_smooth, label='tanh')
+    plt.plot(x_new,sig_smooth, label='sigmoid')
+    # show legend
     plt.legend();
     plt.show()
 
 #Function chooser
-func_arg = {"-q1_1": q1_1, "-q1_2": q1_2}
+func_arg = {"-q1_1": q1_1, "-q1_2": q1_2, "-q5": q5}
 if __name__ == "__main__":
     func_arg[sys.argv[1]]()
