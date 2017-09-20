34f4715dc253ea03e9ec0cbdc4db7ac7a50858f1
Xin Wang <wangxin94@163.com>
2017-09-19 22:40:32 -0400

    load validation set successfully

--- hw1/p6.py
+++ hw1/p6.py
@@ -11,11 +11,27 @@ test_set = "./data/digitstest.txt"
 
 def load_data(training_set, validation_set, test_set):
 
-    #(3000, 784)
-    x_train = np.loadtxt(training_set, delimiter=',',  usecols=range(784))
-    # y_train = np.l
+    ''' Load Training Data'''
+    # (3000, 785)
+    training_data = np.loadtxt(training_set, delimiter=',')
+    train_row = training_data.shape[0]
+    train_col = training_data.shape[1]
+    # (3000, 784)
+    x_train = training_data[:,0:train_col - 1]
+    # (3000, 1)
+    y_train = training_data[:, train_col - 1].reshape(train_row, 1)
+
+    ''' Load Validation Data '''
+    validation_data = np.loadtxt(validation_set, delimiter=',')
+    valid_row = validation_data.shape[0]
+    valid_col = validation_data.shape[1]
+
+    x_valid = validation_data[:,0:valid_col - 1]
+    y_valid = validation_data[:, valid_col - 1].reshape(valid_row, 1)
+
+
     # Visualization
-    print x_train.shape
+    print x_valid.shape, y_valid.shape
     # return x_train, y_train, x_validate, y_validate, x_test, y_test
 
 
