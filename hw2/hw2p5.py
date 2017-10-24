9720a57f8bac6ecf098a414d58bea05ab7a18be8
Xin Wang <wangxin94@163.com>
2017-10-23 20:33:36 -0400

    change gradient computation to a more concise way and add visualization

--- hw2/hw2p5.py
+++ hw2/hw2p5.py
@@ -13,7 +13,7 @@ test_set = "../mnist_data/digitstest.txt"
 # tunable parameters
 cd_steps = 1    # run cd_steps iterations of Gibbs Sampling
 num_hidden_units = 100  # number of hidden units
-epochs = 500
+epochs = 100
 eta = 0.01   # learning rate
 
 # parameters of normal distribution in weights initialization
@@ -28,13 +28,10 @@ def a():
     x_train, y_train = load_data(training_set)     # (3000, 784), (3000, 1)
     # Load Validation Data (1000, 785)
     x_valid, y_valid = load_data(validation_set)    # (1000, 784), (1000, 1)
-    # Load Test Data (3000, 785)
-    x_test, y_test = load_data(test_set)            # (3000, 784), (3000, 1)
 
     # Get number of examples
     num_training_example = x_train.shape[0]
     num_valid_example = x_valid.shape[0]
-    num_test_example = x_test.shape[0]
 
     num_input = x_train.shape[1]
     num_hidden = num_hidden_units
@@ -51,17 +48,18 @@ def a():
         for i in range(num_training_example):
             x = x_train[i, :].reshape(num_input, 1)    # (784, 1)
             # positive phase
-            h = update_hidden(x, hidbias, weights)    # (hidden_units, 1)
-            pos_mean = -1 * np.dot(x, h.T)    # (input, hidden_units)
+            h_probs = update_hidden(x, hidbias, weights)    # (hidden_units, 1)
+            h = get_binary_values(h_probs)
+            pos_mean = np.dot(x, h.T)    # (input, hidden_units)
 
             # negative phase
             h_tilde, x_tilde = gibbs_sampling(x, hidbias, visbias, cd_steps, weights)
-            neg_mean = -1 * np.dot(x_tilde, h_tilde.T)
+            neg_mean = np.dot(x_tilde, h_tilde.T)
 
             # compute gradient
-            weights -= eta * get_gradient(pos_mean, neg_mean)
-            hidbias -= eta * get_gradient(-1 * h, -1 * h_tilde)
-            visbias -= eta * get_gradient(-1 * x, -1 * x_tilde)
+            weights += eta * (pos_mean - neg_mean)
+            hidbias += eta * (h - h_tilde)
+            visbias += eta * (x - x_tilde)
 
             # get cross entropy reconstruction error
             h_recon = update_hidden(x, hidbias, weights)
@@ -87,6 +85,34 @@ def a():
         valid_recon_error_list.append(valid_recon_error_avg)
         print "##### Epoch %s ######\n epoch=%s, eta=%s, hidden_units=%s\n training_error = %s valid_error=%s\n" \
             % (e + 1, epochs, eta, num_hidden_units, train_recon_error_avg, valid_recon_error_avg)
+
+    ''' Visualization '''
+    # Cross Entropy
+    plt.figure(1)
+    plt.xlabel("# epochs")
+    plt.ylabel("error")
+    plt.plot(train_recon_error_list, label='training error')
+    plt.plot(valid_recon_error_list, label='valid error')
+    plt.title('Cross Entropy Reconstruction Error\n (learning rate=%s, hidden_units=%s)'\
+            % (eta, num_hidden_units))
+    plt.legend()
+
+    # weights
+    fig, axs = plt.subplots(10, 10)
+    # Remove horizontal space between axes
+    fig.subplots_adjust(wspace=0, hspace=0)
+    count = 1
+    num_pictures = int(np.sqrt(num_hidden_units))
+    for i in range(num_pictures):
+        for j in range(num_pictures):
+            plt.subplot(num_pictures, num_pictures, count)
+            plt.xticks([])
+            plt.yticks([])
+            plt.imshow(weights[:, count - 1].reshape(28, 28), cmap='gray', origin='lower')
+            count += 1
+
+    plt.show()
+
 # Calculate cross entropy
 def cross_entropy(o, y):
     """
@@ -96,11 +122,9 @@ def cross_entropy(o, y):
         Output:
             cross entropy of this example
     """
-    bias = 0
+    bias = np.exp(-20)
     return np.sum(np.nan_to_num(-y * np.log(o + bias) - (1-y) * np.log(1-o + bias)))
 
-def get_gradient(pos, neg):
-    return pos - neg
 
 def gibbs_sampling(vis, hidbias, visbias, steps, weights):
     '''
@@ -127,7 +151,7 @@ def gibbs_sampling(vis, hidbias, visbias, steps, weights):
 def get_binary_values(probs):
     samples = np.random.uniform(size=probs.shape)
     probs[samples < probs] = 1.
-    # set any other non-1 values to 0
+    # set any other non-one values to 0
     np.floor(probs, probs)
     return probs
 
