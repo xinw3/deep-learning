f373d7cbac46eed861c681d81ff5fbe7128ef081
Xin Wang <wangxin94@163.com>
2017-11-12 20:00:51 -0500

    load the dict file into memory

--- hw3/code/4gram_mapper.py
+++ hw3/code/4gram_mapper.py
@@ -5,22 +5,28 @@ import re
 
 def mapper():
     '''
-        Input: output7997   (words,count)
+        Build 4-grams for train.txt
+
+        Input: train.txt
                top 7997 words in the training file to build vocabulary
     '''
+    voc_file_name = 'output7997'
     word_dict = dict()
     tags = ['UNK', 'START', 'END']
     # build word vocabulary
+    print 'building vocabulary...'
     index = 0
-    for line in sys.stdin:
-        word, count = line.split(',')
-        word_dict[index] = word
-        index += 1
+    with open(voc_file_name) as f:
+        for line in f:
+            word, count = line.split(',')
+            word_dict[index] = word
+            index += 1
 
     for i in range(len(tags)):
         word_dict[index + i] = tags[i]
 
     print word_dict
+    print 'done'
 
 if __name__ == "__main__":
     mapper()
