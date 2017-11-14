da6c0ab270c65cf600f7ae385697627e07400434
Xin Wang <wangxin94@163.com>
2017-11-13 20:16:03 -0500

    clean up 4-grams

--- hw3/code/plot_ngram.py
+++ hw3/code/plot_ngram.py
@@ -9,24 +9,17 @@ def plot_ngram():
     '''
         Build 4-grams for train.txt
 
-        Input: train.txt
-               top 7997 words in the training file to build vocabulary
+        Input: output_ngram
+               build list for 4-grams
     '''
     voc_file_name = 'output_ngram'
     count_list = []
-
-    threshold = 2
 
     with open(voc_file_name) as f:
         for line in f:
             word, count = line.split('\t')
             count = int(count)
             count_list.append(count)
-            if count < threshold:
-                break
-
-
-    x_axis = np.arange(len(count_list))
 
     plt.plot(count_list)
     plt.xlabel('id')
